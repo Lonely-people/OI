9606106db643c1f555611171f0882c3b24d837c2
Lonely-people <45394200+Lonely-people@users.noreply.github.com>
2019-06-16 09:40:58 +0800

    Update 最大流.cpp

--- 模板/最大流.cpp
+++ 模板/最大流.cpp
@@ -1,109 +1,48 @@
-#include<cstdio>
-#define INF 0x3f3f3f3f
-#define maxn 3000001
-#define _min(a, b) a < b ? a : b
-#define _max(a, b) a > b ? a : b
-#define _abs(a) a < 0 ? -a : a
-#define cmax(a, b) (a > b) && (a = b)
-#define cmin(a, b) (a < b) && (a = b)
-inline int read(){
-    int r=0,f=1;
-    char c=getchar();
-    while(c<'0'||c>'9'){
-        if(c=='-')f=-1;
-        c=getchar();
-    }
-    while(c>='0'&&c<='9'){
-        r=r*10+c-'0';
-        c=getchar();
-    }
-    return r*f;
-}
-void write(int x)
-{
-    if(x<0)
-        putchar('-'),x=-x;
-    if(x>9)
-        write(x/10);
-    putchar(x%10+'0');
-}
-struct edge{
-    int v,c,nxt;
-}e[2*maxn];
-    int S,T,N,head[maxn],q[maxn],cur[maxn],level[maxn],tot=0,hd,tl,v;
-    inline void init(int s,int t,int n){
-        S=s;
-        T=t;
-        N=n+1;
-        for(register int i=1;i<N;i++)
-            head[i]=-1;
-    }
-    inline void add_edge(int u,int v,int c){
-        e[tot]=(edge){v,c,head[u]};
-        head[u]=tot++;
-    } 
-    inline void add(int u,int v,int c){
-        add_edge(u,v,c);
-        add_edge(v,u,0);
-    }
-    inline bool BFS(){
-        for(register int i=1;i<N;i++){
-            cur[i]=head[i];
-            level[i]=0;
-        }
-        q[1]=S;
-        level[S]=1;
-        hd=0;tl=1;
-        while(hd^tl){
-            hd++;
-            for(register int i=head[q[hd]];i!=-1;i=e[i].nxt){
-                if(e[i].c&&!level[e[i].v]){
-                    tl++;
-                    level[e[i].v]=level[q[hd]]+1;
-                    q[tl]=e[i].v;
-                    if(e[i].v==T)return true;
-                }
-            }
-        }
-        return false;
-    }
-    inline int min(int a,int b){
-        if(a<b)return a;
-        return b;
-    }
-    int DFS(int f,int u){
-        if(u==T)return f;
-        int d=0,used=0;
-        for(int &i=cur[u];i!=-1;i=e[i].nxt){
-            if(e[i].c&&level[u]==level[e[i].v]-1){
-                if((d=DFS(min(f-used,e[i].c),e[i].v))){
-                    e[i].c-=d;
-                    e[i^1].c+=d;
-                    used+=d;
-                }
-            }
-        }
-        if(!used)level[u]=0;
-        return used;
-    }
-    inline int Dinic(){
-        int max_flow=0;
-        while(BFS()){
-            int d=0;
-            while((d=DFS(INF,S)))
-                max_flow+=d;
-        }
-        return max_flow;
-    }
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+#define reg register
+#define fp(i, a, b) for(reg int i = (a); i <= (b); ++i)
+#define fd(i, a, b) for(reg int i = (a); i >= (b); i--)
 
-int main(){
-    int n=read(),m=read(),s=read(),t=read(),i;
-    init(s,t,n);
-    for(i=1;i<=m;i++){
-        int a=read(),b=read(),v=read();
-        add(a,b,v);
-    }
-    int ans=Dinic();
-    write(ans);
-    return 0;
+using namespace std;
+
+const int MAXN = 20010;
+
+int s, t, dis[MAXN], dep[MAXN], n, m, u, v, c;
+
+struct edge {
+	edge * next, * rev;
+	int to, v;
+}pool[MAXN], * pt[MAXN], * p = pool, * cur[MAXN];
+
+inline void add(int a, int b, int c) {
+	* (++p) = (edge) { pt[a], p + 1, b, c }, pt[a] = p;
+	* (++p) = (edge) { pt[b], p - 1, a, 0 }, pt[b] = p;
+}
+
+int maxflow(int u, int val) {
+	if(u == t) return val;
+	int v = val;
+	for(; cur[u]; cur[u] = cur[u] -> next) {
+		if(dis[cur[u] -> to] == dis[u] - 1 && cur[u] -> v) {
+			int f = maxflow(cur[u] -> to, min(v, cur[u] -> v));
+			cur[u] -> v -= f, cur[u] -> rev -> v += f, v -= f;
+		    if(!v) return val;
+		}
+	}
+	if(!--dep[dis[u]++]) dis[s] = n + 1;
+	++dep[dis[u]], cur[u] = pt[u];
+	return val - v;
+}
+
+int main() {
+	scanf("%d%d%d%d", &n, &m, &s, &t);
+	fp(i, 1, m) scanf("%d%d%d", &u, &v, &c), add(u, v, c);
+	long long ans = 0;
+	fp(i, 1, n) cur[i] = pt[i];
+	for (dep[0] = n; dis[s] <= n; ans += maxflow(s, 0x3f3f3f3f));
+	printf("%lld\n", ans);
+	return 0;
 }
